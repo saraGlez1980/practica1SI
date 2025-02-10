0bb3eae48ee784745079550df2026f6f4bc9af98
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-02-10 10:41:44 +0100

    main en clase demo

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -1,5 +1,9 @@
 package demo;
-public class demo{
-	
+public class demo {
+	public static void main() {
+		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
+}
 }
 
